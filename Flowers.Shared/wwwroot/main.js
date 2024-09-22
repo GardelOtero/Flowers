5b53f927b51007e5c7087e73e391a3f026685570
Gardel Alejandro Otero Verdugo <angrygaru@gmail.com>
2024-09-21 18:34:01 -0700

    flores listas

--- Flowers.Shared/wwwroot/main.js
+++ Flowers.Shared/wwwroot/main.js
@@ -3,4 +3,11 @@ onload = () => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
     }, 1000);
-  };
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.body.classList.remove('not-loaded');
+        document.body.classList.add('loaded');
+    }, 4000); // Ajustar el tiempo para asegurarse de que las flores terminen de cargarse
+});
